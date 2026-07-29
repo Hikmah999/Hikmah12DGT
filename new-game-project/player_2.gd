c8837581960d77a41c5886eafe557d0cffdae4dc
Hikmah999 <23206@students.mrgs.school.nz>
2026-07-30 10:52:44 +1200

    new characetr

---
+++ new-game-project/player_2.gd
@@ -0,0 +1,36 @@
+extends CharacterBody2D
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# Gravity
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Jump
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Movement
+	var direction := Input.get_axis("ui_left", "ui_right")
+
+	if direction != 0:
+		velocity.x = direction * SPEED
+
+		# Face the opposite direction
+		animated_sprite.flip_h = direction > 0
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	# Animations
+	if not is_on_floor():
+		animated_sprite.play("jump")
+	elif direction != 0:
+		animated_sprite.play("walk")
+	else:
+		animated_sprite.play("standing")
+
+	move_and_slide()
