206162be437d63c15a83af7065434ea76d9ef9a9
Hikmah999 <23206@students.mrgs.school.nz>
2026-08-03 15:06:03 +1200

    new characters

--- new-game-project/player_2.gd
+++ new-game-project/player_2.gd
@@ -6,7 +6,7 @@ const JUMP_VELOCITY = -400.0
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	# Gravity
+	# Apply gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -14,18 +14,18 @@ func _physics_process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Movement
+	# Get movement input
 	var direction := Input.get_axis("ui_left", "ui_right")
 
 	if direction != 0:
 		velocity.x = direction * SPEED
 
-		# Face the opposite direction
-		animated_sprite.flip_h = direction > 0
+		# Flip sprite when moving left
+		animated_sprite.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	# Animations
+	# Play animations
 	if not is_on_floor():
 		animated_sprite.play("jump")
 	elif direction != 0:
