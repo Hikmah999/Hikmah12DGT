29d4295c8c1f6c00123c0bd1a9f3bbe2ba2410bb
Hikmah999 <23206@students.mrgs.school.nz>
2026-08-19 13:48:41 +1200

    new

--- new-game-project/Npcs/npc_l_3_1.gd
+++ new-game-project/Npcs/npc_l_3_1.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+var is_happy: bool = false
 
-func _ready() -> void:
-	animated_sprite.play("standing")
-
-func _physics_process(_delta: float) -> void:
-	velocity = Vector2.ZERO
-	move_and_slide()
+func  make_happy() -> void:
+	if is_happy:
+		return
+	
+	is_happy = true
+	$AnimatedSprite2D.play("happy")
